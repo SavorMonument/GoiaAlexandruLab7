4ba82d4bb7073701da25286a34f90aed9d61ce8b
j <j@DESKTOP-VUCHEVQ>
2026-01-11 22:12:35 +0200

    Add project files.

---
+++ GoiaAlexandruLab7/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GoiaAlexandruLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
