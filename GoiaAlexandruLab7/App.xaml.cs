ca56f0bbd7a0d77a4623dcc8c44d4225ec754bf3
savormonument <savormonument@gmail.com>
2026-01-11 23:10:04 +0200

    Shopping list pasge with creation

--- GoiaAlexandruLab7/App.xaml.cs
+++ GoiaAlexandruLab7/App.xaml.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using GoiaAlexandruLab7.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GoiaAlexandruLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(
+                        Path.Combine(FileSystem.AppDataDirectory, "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
